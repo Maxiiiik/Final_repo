4160467e21a3aa405553266b02c859639aec7809
Maxiiiik <shuprudkomax00@gmail.com>
2021-03-04 15:04:31 +0200

    [Final_git] edit 1.cpp

--- 1.cpp
+++ 1.cpp
@@ -4,8 +4,11 @@ using namespace std;
 
 int main()
 {
-    //1
-    /*{
+    /*
+	Напишите программу, которая выполняет 
+	поэлементную сумму двух массивов и результат заносит в третий массив.
+	*/
+    {
         const int size = 2;
         int arr1[size];
         int arr2[size];
@@ -31,90 +34,8 @@ int main()
             cout << "arrSum = ";
             cout << arrSum[i] << "\n";
         }
-    }*/
-    //2
-    /*{
-        const int size = 3;
-        int arr[size];
-        int Sum , Number;
-        for (int i = 0; i < size; i++)
-        {
-            cout << "arr[" << i + 1 << "] = ";
-            cin >> arr[i];
-        }
-        for (int i = 0; i < size; i++)
-        {
-            if (arr[i] == 0)
-            {
-                Number = i;
-                break;
-            }
-        }
-        Sum = 0;
-        for (int i = Number; i < size; i++)
-        {
-            Sum += arr[i];
-        }
-        cout << "\nSum = " << Sum << endl;
-    }*/
-    //3
-    /*{
-        const int size = 5;
-        int arr1[size];
-        int arr2[size];
-        int Сoincided = 0 , Counter;
-
-        //array User
-        cout << "User : " << endl;
-        for (int i = 0; i < size; i++)
-        {
-            cout << "arr[" << i + 1 << "] = ";
-            cin >> arr1[i];
-        }
-        cout << endl;
-
-        //array Lottery
-        for (int i = 0; i < size; i++)
-        {
-            arr2[i] = 0 + rand() % 9;
-        }
-        cout << endl;
-
-        // Output arrays
-        cout << "User arr: ";
-        for (size_t i = 0; i < size; i++)
-        {
-            cout << "\t" << arr1[i];
-        }
-        cout << endl;
-        cout << "Lottery arr: ";
-        for (size_t i = 0; i < size; i++)
-        {
-            cout << "\t" << arr2[i];
-        }
-
-        // search coincided
-        for (size_t i = 0; i < size; i++)
-        {
-            Counter = 0;
-                if (arr1[i] == arr2[i])
-                {
-                    Counter++;
-                    Сoincided += Counter;
-                }
-            
-        }
-        if (Сoincided % 5 == 0)
-        {
-            cout << "\nYou SUPER WINNER!!!!!!";
-            cout << "\nCOINCIDED = " << Сoincided;
-        }
-        else
-        {
-            cout << "\nCoincided = " << Сoincided << endl;
-            cout << "Luck next time!!"<< endl;
-        }
-    }*/
+    }
+    
 	cout << endl;
 	system("pause");
 	return 0;
